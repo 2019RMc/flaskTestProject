64c58ace92cfd16aae4e6747e4daea98d28420bb
rjm <rjm@outlook.com.au>
2024-02-02 23:41:58 +1100

    start ep7

--- hello.py
+++ hello.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app=Flask(__name__)
+app.config['SECRET_KEY'] = 'wombat'
+
+# create a form class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 @app.route('/')
 
@@ -41,6 +51,20 @@ def page_not_found(e):
 @app.errorhandler(500)
 def Internal_Server_Error(e):
     return render_template("500.html"), 500
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name=None
+    form = NamerForm()
+
+    if form.validate_on_submit():
+        name=form.name.data
+        form.name.data = ''
+        flash('Form Submitted Successfully')
+
+    return render_template('name.html',
+                           name=name,
+                           form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
